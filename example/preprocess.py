a17a58a0afb9222c07d979f0ed453ace752818dd
unknown <gonasravani27@gmail.com>
2024-12-04 22:56:22 -0500

    Updating example queries

--- example/preprocess.py
+++ example/preprocess.py
@@ -1,16 +1,8 @@
 from flask_ml.flask_ml_client import MLClient
 from flask_ml.flask_ml_server.models import DirectoryInput, Input
 
-# Path to the COCO-128 image dataset
-# dataset_name = "coco"
-# image_dir = './coco/train'  # Update with your image path
-
-# # Disable SSL certificate verification
-# ssl._create_default_https_context = ssl._create_unverified_context
-
-# clip_model = CLIPModel()
-# clip_model.preprocess_images(image_dir, dataset_name)
-
+# Path to the test dataset
+dataset_path = './datasets/coco/'
 
 url = "http://127.0.0.1:5000/dataset_processing"  # The URL of the server
 client = MLClient(url)  # Create an instance of the MLClient object
@@ -18,12 +10,12 @@ client = MLClient(url)  # Create an instance of the MLClient object
 inputs = {
     "input_dir": Input(
         root=DirectoryInput.model_validate(
-            {"path": "./coco/train"}
+            {"path": dataset_path}
         )
     )
 }
 parameters = {
-    "dataset_name": "coco"
+    "dataset_name": "clientExampleDataset"
 }
 response = client.request(inputs, parameters)  # Send a request to the server
 print(response)  # Print the response
